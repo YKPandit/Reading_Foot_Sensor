da59cb79de45107b45b72f684126354a00fbfdf2
YKPandit <yaaskakpandit@gmail.com>
2025-09-29 16:04:01 -0700

    added the fix for interval stuff

--- reading_foot_force/reading_foot_force/lowStateSub.py
+++ reading_foot_force/reading_foot_force/lowStateSub.py
@@ -2,10 +2,8 @@ import rclpy
 from rclpy.node import Node
 from unitree_go.msg import LowState
 from std_msgs.msg import String
-from time import perf_counter_ns
-from time import time
+from time import perf_counter_ns, time
 
-# Constructor that inherits from the main Node class
 class MinSub(Node):
     def __init__(self):
         super().__init__('min_sub')
@@ -15,19 +13,28 @@ class MinSub(Node):
         self.log_hz = 30
         self.t_delta_ns = 1_000_000_000 / self.log_hz
         self.t_start = perf_counter_ns()
+        self.data_buffer = []
+        self.interval_sec = 3
+        self.interval_start = time()
+        self.file_count = 0
 
     def listener_callback(self, msg):
         t_end = perf_counter_ns()
         if t_end - self.t_start > self.t_delta_ns:
             self.t_start = t_end
 
-            # log is of the form: <timestamp>,<f1>,<f2>,<f3>,<f4>
             timestamp_ms = int(time() * 1000)
             log = str(timestamp_ms) + ',' + ','.join(map(str, msg.foot_force))
-            print(log)
+            self.data_buffer.append(log)
 
-            # old log:
-            # self.get_logger().info(f"Recieved: {msg.foot_force}")
+            # Check if 3 seconds have passed
+            if time() - self.interval_start >= self.interval_sec:
+                filename = f"foot_force_{self.file_count}_{timestamp_ms}.csv"
+                with open(filename, "w") as f:
+                    f.write('\n'.join(self.data_buffer))
+                self.data_buffer = []
+                self.file_count += 1
+                self.interval_start = time()
 
 def main():
     rclpy.init()
